caa431dc0068541fe96c9a9b92b7e5aa7fb6f44e
winning godspower <winninggodspower@gmail.com>
2022-11-15 07:31:48 +0100

    added a requirements.txt file

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
+
+file_path = os.path.abspath(os.getcwd())+"\database.db"
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'f6d89eb8472ebf47c276e0f40171e13e'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/user.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
 app.config["USER_IMG_PATH"] = "static/profile_pics"
 
 db = SQLAlchemy(app)
