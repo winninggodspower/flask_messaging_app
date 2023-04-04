31d19ee1c3d16a5464175c9bdf6e83067da2176c
winning godspower <winninggodspower@gmail.com>
2023-04-04 11:16:58 +0100

    added loading git

--- app/static/js/like.js
+++ app/static/js/like.js
@@ -9,11 +9,23 @@ const likeStateContent = {
     unlike: '<i class="bi bi-hand-thumbs-up"></i>'
 };
 
+const likeStateCount = {
+    like: +1,
+    unlike: -1
+};
+
+document.querySelectorAll('.loading-gif').forEach(gif=>{
+    gif.style.display = "none";
+})
 
 likeButtons.forEach((btn)=>{
 
     btn.addEventListener('click', ()=>{
-        fetch(`/post/like/${btn.dataset.likeId}`, {
+        let postId = btn.dataset.likeId
+        let loadingGif = document.getElementById(postId+'-loading-gif');
+        loadingGif.style.display = "inline";
+
+        fetch(`/post/like/${postId}`, {
             method: "POST",
         })
         .then(res=>res.json())
@@ -21,11 +33,18 @@ likeButtons.forEach((btn)=>{
             if (data.message === "success"){
                 btn.style.color = likeStateColor[data.action];
                 btn.innerHTML = likeStateContent[data.action];
+                
+                // change like count
+                let likeCount = document.getElementById(`${postId}-like-count`)
+                let newLikeCount = parseInt(likeCount.innerText) + likeStateCount[data.action];
+                likeCount.innerText = newLikeCount;
             }
             else{
                 location.replace(data.redirect_url) 
             }
+            loadingGif.style.display = "none"
         })
+
     })
 
 })
